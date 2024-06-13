b0b0c588b097e90e31971d37506700db47af2bb8
quangbv <quangbv@nlbn.net>
2024-06-13 16:07:47 +0700

    try catch call data

--- lib/call_data/network_request.dart
+++ lib/call_data/network_request.dart
@@ -28,30 +28,35 @@ class NetworkRequest {
     /// https://language-master.top/api/getidv2/ca-app-pub-4973559944609228~2346710863+com.example.lib
     var url = linkServer != null && appId != null && packageName != null
         ? Uri.parse('$linkServer/api/getidv2/$appId+$packageName')
-        : Uri.parse('https://language-master.top/api/getidv2/ca-app-pub-4973559944609228~2346710863');
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      print('body: ${response.body}');
-      List<AdsModel> listAds = await compute(parseAdsModel, response.body);
+        : Uri.parse(
+            'https://language-master.top/api/getidv2/ca-app-pub-4973559944609228~2346710863');
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        print('body: ${response.body}');
+        List<AdsModel> listAds = await compute(parseAdsModel, response.body);
 
-      for (final model in listAds) {
-        if (model.name != null) {
-          // Khởi tạo danh sách nếu chưa tồn tại
-          listAdsId.putIfAbsent(model.name!, () => []);
+        for (final model in listAds) {
+          if (model.name != null) {
+            // Khởi tạo danh sách nếu chưa tồn tại
+            listAdsId.putIfAbsent(model.name!, () => []);
 
-          if (model.adsId != null) {
-            listAdsId[model.name!]!.add(model.adsId!);
+            if (model.adsId != null) {
+              listAdsId[model.name!]!.add(model.adsId!);
+            }
           }
         }
-      }
 
-      onResponse.call();
-    } else if (response.statusCode == 404) {
+        onResponse.call();
+      } else if (response.statusCode == 404) {
+        onError.call();
+        throw Exception('Not Found');
+      } else {
+        onError.call();
+        throw Exception('Can\'t get ads id');
+      }
+    } catch (e) {
       onError.call();
-      throw Exception('Not Found');
-    } else {
-      onError.call();
-      throw Exception('Can\'t get ads id');
     }
   }
 
