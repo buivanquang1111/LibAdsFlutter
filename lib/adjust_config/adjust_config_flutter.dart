6f8c52313d981e17aecb22c174db59649db73025
quangbv <quangbv@nlbn.net>
2024-06-24 10:29:40 +0700

    adjust track revenue

--- lib/adjust_config/adjust_config_flutter.dart
+++ lib/adjust_config/adjust_config_flutter.dart
@@ -1,5 +1,8 @@
 import 'package:adjust_sdk/adjust.dart';
+import 'package:adjust_sdk/adjust_ad_revenue.dart';
 import 'package:adjust_sdk/adjust_config.dart';
+import 'package:adjust_sdk/adjust_event.dart';
+import 'package:amazic_ads_flutter/src/utils/ad_event.dart';
 
 class AdjustConfigFlutter{
   AdjustConfigFlutter._instance();
@@ -12,4 +15,32 @@ class AdjustConfigFlutter{
     config.defaultTracker = adjustToken;
     Adjust.start(config);
   }
+
+  void trackRevenue(AdEvent event) {
+    final Map<String, dynamic>? data = event.data as Map<String, dynamic>?;
+
+    final num? revenue = data?['revenue'];
+    final String? currencyCode = data?['currencyCode'];
+    final String? network = data?['network'];
+    final String? unit = data?['unit'];
+    final String? placement = data?['placement'];
+
+    if (revenue != null && currencyCode != null) {
+      AdjustAdRevenue adRevenue;
+      switch (event.adNetwork) {
+        default:
+          adRevenue = AdjustAdRevenue(AdjustConfig.AdRevenueSourceAdMob);
+          break;
+      }
+      adRevenue.setRevenue(revenue, currencyCode);
+      adRevenue.adRevenueNetwork = network;
+      adRevenue.adRevenueUnit = unit;
+      adRevenue.adRevenuePlacement = placement;
+      Adjust.trackAdRevenueNew(adRevenue);
+      // final adjustEvent = AdjustEvent("f5l43s");
+      // adjustEvent.setRevenue(revenue, currencyCode);
+      // Adjust.trackEvent(adjustEvent);
+    }
+  }
+
 }
