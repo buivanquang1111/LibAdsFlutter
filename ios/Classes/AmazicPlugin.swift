e3c87935c9e89dbb67e1110931253b09b661d00c
quangbv <quangbv@nlbn.net>
2024-06-25 18:16:36 +0700

    upp

--- ios/Classes/AmazicPlugin.swift
+++ ios/Classes/AmazicPlugin.swift
@@ -2,76 +2,105 @@ import Flutter
 import UIKit
 
 public class AmazicPlugin: NSObject, FlutterPlugin {
-    static let TAG = "AmazicPlugin"
-    private var context: UIApplication!
-    private var mActivity: UIViewController!
-    private var dialog: FullscreenLoadingDialog?
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "easy_ads_flutter", binaryMessenger: registrar.messenger())
+    let instance = AmazicPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
 
-    private static var channel: FlutterMethodChannel?
-    public static var loadingChannel: FlutterMethodChannel?
-
-
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        if channel == nil {
-            channel = FlutterMethodChannel(name: "easy_ads_flutter", binaryMessenger: registrar.messenger())
-            let instance = AmazicPlugin()
-            registrar.addMethodCallDelegate(instance, channel: channel!)
-        }
-
-        if loadingChannel == nil {
-            loadingChannel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: registrar.messenger())
-        }
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
+    case "hasConsentForPurposeOne":
+       result(true)
+    case "showLoadingAd":
+        break;
+       //result(true)
+    case "hideLoadingAd":
+        break;
+      //result(true)
+    default:
+      result(FlutterMethodNotImplemented)
     }
-
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch call.method {
-        case "getPlatformVersion":
-            result("iOS " + UIDevice.current.systemVersion)
-        case "hasConsentForPurposeOne":
-            hasConsentForPurposeOne(result: result)
-        case "showLoadingAd":
-            if let colorValue = call.arguments as? Int {
-                showOverlayLoading(color: UIColor(hex: colorValue))
-            }
-        case "hideLoadingAd":
-            hideOverlayLoading()
-        default:
-            result(FlutterMethodNotImplemented)
-        }
-    }
-
-    private func hasConsentForPurposeOne(result: FlutterResult) {
-        let defaults = UserDefaults.standard
-        if let purposeConsents = defaults.string(forKey: "IABTCF_PurposeConsents") {
-            let purposeOneString = String(purposeConsents.prefix(1))
-            let hasConsentForPurposeOne = purposeOneString == "1"
-            result(hasConsentForPurposeOne)
-        } else {
-            result(nil)
-        }
-    }
-
-    private func showOverlayLoading(color: UIColor) {
-        if dialog == nil {
-            dialog = FullscreenLoadingDialog(color: color)
-            dialog?.modalPresentationStyle = .overFullScreen
-            mActivity.present(dialog!, animated: true, completion: nil)
-        }
-    }
-
-    private func hideOverlayLoading() {
-        if dialog != nil {
-            dialog?.dismiss(animated: true, completion: nil)
-            dialog = nil
-        }
-    }
+  }
 }
 
-extension UIColor {
-    convenience init(hex: Int) {
-        let red = CGFloat((hex >> 16) & 0xFF) / 255.0
-        let green = CGFloat((hex >> 8) & 0xFF) / 255.0
-        let blue = CGFloat(hex & 0xFF) / 255.0
-        self.init(red: red, green: green, blue: blue, alpha: 1.0)
-    }
-}
+//import Flutter
+//   import UIKit
+//
+//   public class AmazicPlugin: NSObject, FlutterPlugin {
+//       static let TAG = "AmazicPlugin"
+//       private var context: UIApplication!
+//       private var mActivity: UIViewController!
+//       private var dialog: FullscreenLoadingDialog?
+//
+//       private static var channel: FlutterMethodChannel?
+//       public static var loadingChannel: FlutterMethodChannel?
+//
+//
+//       public static func register(with registrar: FlutterPluginRegistrar) {
+//           if channel == nil {
+//               channel = FlutterMethodChannel(name: "easy_ads_flutter", binaryMessenger: registrar.messenger())
+//               let instance = AmazicPlugin()
+//               registrar.addMethodCallDelegate(instance, channel: channel!)
+//           }
+//
+//           if loadingChannel == nil {
+//               loadingChannel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: registrar.messenger())
+//           }
+//       }
+//
+//       public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+//           switch call.method {
+//           case "getPlatformVersion":
+//               result("iOS " + UIDevice.current.systemVersion)
+//           case "hasConsentForPurposeOne":
+//               hasConsentForPurposeOne(result: result)
+//           case "showLoadingAd":
+//               if let colorValue = call.arguments as? Int {
+//                   showOverlayLoading(color: UIColor(hex: colorValue))
+//               }
+//           case "hideLoadingAd":
+//               hideOverlayLoading()
+//           default:
+//               result(FlutterMethodNotImplemented)
+//           }
+//       }
+//
+//       private func hasConsentForPurposeOne(result: FlutterResult) {
+//           let defaults = UserDefaults.standard
+//           if let purposeConsents = defaults.string(forKey: "IABTCF_PurposeConsents") {
+//               let purposeOneString = String(purposeConsents.prefix(1))
+//               let hasConsentForPurposeOne = purposeOneString == "1"
+//               result(hasConsentForPurposeOne)
+//           } else {
+//               result(nil)
+//           }
+//       }
+//
+//       private func showOverlayLoading(color: UIColor) {
+//           if dialog == nil {
+//               dialog = FullscreenLoadingDialog(color: color)
+//               dialog?.modalPresentationStyle = .overFullScreen
+//               mActivity.present(dialog!, animated: true, completion: nil)
+//           }
+//       }
+//
+//       private func hideOverlayLoading() {
+//           if dialog != nil {
+//               dialog?.dismiss(animated: true, completion: nil)
+//               dialog = nil
+//           }
+//       }
+//   }
+//
+//   extension UIColor {
+//       convenience init(hex: Int) {
+//           let red = CGFloat((hex >> 16) & 0xFF) / 255.0
+//           let green = CGFloat((hex >> 8) & 0xFF) / 255.0
+//           let blue = CGFloat(hex & 0xFF) / 255.0
+//           self.init(red: red, green: green, blue: blue, alpha: 1.0)
+//       }
+//   }
+//
--- ios/Classes/FullscreenLoadingDialog.swift
+++ ios/Classes/FullscreenLoadingDialog.swift
@@ -1,108 +1,108 @@
-import UIKit
-import Flutter
-
-class FullscreenLoadingDialog: UIViewController {
-
-    static let TAG = "FullscreenLoadingDialog"
-
-    var adFailedToShow = false
-    var isResume = true
-    let color: UIColor
-    var progressBar: UIActivityIndicatorView!
-
-    init(color: UIColor) {
-        self.color = color
-        super.init(nibName: nil, bundle: nil)
-        print(FullscreenLoadingDialog.TAG, color)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = .white
-
-        progressBar = UIActivityIndicatorView(style: .large)
-        progressBar.color = color
-        progressBar.startAnimating()
-        progressBar.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(progressBar)
-
-        NSLayoutConstraint.activate([
-            progressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            progressBar.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-
-        NotificationCenter.default.addObserver(self, selector: #selector(onAppBackgrounded), name: UIApplication.didEnterBackgroundNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(onAppForegrounded), name: UIApplication.willEnterForegroundNotification, object: nil)
-    }
-
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print(FullscreenLoadingDialog.TAG, "onStart")
-
-        if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
-            let channel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: flutterEngine.binaryMessenger)
-            channel.setMethodCallHandler { [weak self] (call, result) in
-                switch call.method {
-                case "handleShowAd":
-                    self?.showAd()
-                    result(nil)
-                case "closeAd":
-                    self?.closeAd()
-                    result(nil)
-                default:
-                    result(FlutterMethodNotImplemented)
-                }
-            }
-        }
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print(FullscreenLoadingDialog.TAG, "onStop")
-
-        if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
-            let channel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: flutterEngine.binaryMessenger)
-            channel.setMethodCallHandler(nil)
-        }
-    }
-
-    func showAd() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            if self.isResume {
-                if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
-                    let channel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: flutterEngine.binaryMessenger)
-                    channel.invokeMethod("showAd", arguments: nil)
-                }
-            } else {
-                self.adFailedToShow = true
-            }
-        }
-    }
-
-    func closeAd() {
-        self.dismiss(animated: true, completion: nil)
-    }
-
-    @objc func onAppBackgrounded() {
-        print(FullscreenLoadingDialog.TAG, "onAppBackgrounded")
-        isResume = false
-    }
-
-    @objc func onAppForegrounded() {
-        print(FullscreenLoadingDialog.TAG, "onAppForegrounded")
-        isResume = true
-        if adFailedToShow {
-            showAd()
-        }
-    }
-}
+// import UIKit
+// import Flutter
+//
+// class FullscreenLoadingDialog: UIViewController {
+//
+//     static let TAG = "FullscreenLoadingDialog"
+//
+//     var adFailedToShow = false
+//     var isResume = true
+//     let color: UIColor
+//     var progressBar: UIActivityIndicatorView!
+//
+//     init(color: UIColor) {
+//         self.color = color
+//         super.init(nibName: nil, bundle: nil)
+//         print(FullscreenLoadingDialog.TAG, color)
+//     }
+//
+//     required init?(coder: NSCoder) {
+//         fatalError("init(coder:) has not been implemented")
+//     }
+//
+//     override func viewDidLoad() {
+//         super.viewDidLoad()
+//
+//         view.backgroundColor = .white
+//
+//         progressBar = UIActivityIndicatorView(style: .large)
+//         progressBar.color = color
+//         progressBar.startAnimating()
+//         progressBar.translatesAutoresizingMaskIntoConstraints = false
+//
+//         view.addSubview(progressBar)
+//
+//         NSLayoutConstraint.activate([
+//             progressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//             progressBar.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//         ])
+//
+//         NotificationCenter.default.addObserver(self, selector: #selector(onAppBackgrounded), name: UIApplication.didEnterBackgroundNotification, object: nil)
+//         NotificationCenter.default.addObserver(self, selector: #selector(onAppForegrounded), name: UIApplication.willEnterForegroundNotification, object: nil)
+//     }
+//
+//     deinit {
+//         NotificationCenter.default.removeObserver(self)
+//     }
+//
+//     override func viewWillAppear(_ animated: Bool) {
+//         super.viewWillAppear(animated)
+//         print(FullscreenLoadingDialog.TAG, "onStart")
+//
+//         if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
+//             let channel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: flutterEngine.binaryMessenger)
+//             channel.setMethodCallHandler { [weak self] (call, result) in
+//                 switch call.method {
+//                 case "handleShowAd":
+//                     self?.showAd()
+//                     result(nil)
+//                 case "closeAd":
+//                     self?.closeAd()
+//                     result(nil)
+//                 default:
+//                     result(FlutterMethodNotImplemented)
+//                 }
+//             }
+//         }
+//     }
+//
+//     override func viewDidDisappear(_ animated: Bool) {
+//         super.viewDidDisappear(animated)
+//         print(FullscreenLoadingDialog.TAG, "onStop")
+//
+//         if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
+//             let channel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: flutterEngine.binaryMessenger)
+//             channel.setMethodCallHandler(nil)
+//         }
+//     }
+//
+//     func showAd() {
+//         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//             if self.isResume {
+//                 if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
+//                     let channel = FlutterMethodChannel(name: "loadingChannel", binaryMessenger: flutterEngine.binaryMessenger)
+//                     channel.invokeMethod("showAd", arguments: nil)
+//                 }
+//             } else {
+//                 self.adFailedToShow = true
+//             }
+//         }
+//     }
+//
+//     func closeAd() {
+//         self.dismiss(animated: true, completion: nil)
+//     }
+//
+//     @objc func onAppBackgrounded() {
+//         print(FullscreenLoadingDialog.TAG, "onAppBackgrounded")
+//         isResume = false
+//     }
+//
+//     @objc func onAppForegrounded() {
+//         print(FullscreenLoadingDialog.TAG, "onAppForegrounded")
+//         isResume = true
+//         if adFailedToShow {
+//             showAd()
+//         }
+//     }
+// }
