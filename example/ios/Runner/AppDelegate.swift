db26fa3a0af84afc6f9d45bd3f2c519e9c1c872f
cuongnl <cuongnl@vtdglobal.com>
2024-06-26 15:15:48 +0700

    update example

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,6 +7,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let methodChannel = FlutterMethodChannel(name: "channel", binaryMessenger: controller.binaryMessenger)
+      methodChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          guard call.method == "init_mediation" else {
+              result(FlutterMethodNotImplemented)
+              return
+          }
+          result(true)
+      })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
